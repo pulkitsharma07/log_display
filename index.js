4518f68753d6f12979c7e8e35579799eaafa21e5
Pulkit Sharma <avastpulkit@gmail.com>
2016-08-03 22:30:57 +0530

    fixed indentation

--- index.js
+++ index.js
@@ -3,40 +3,29 @@ var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
-
-
-
-var send_delta_log = function(socket){
-
+var send_delta_log = function(socket) {
 	fs.readFile("log_file.log", "utf8", function(err, data) {
-        if (err) throw err;
-
-       socket.emit("change",data.substr(socket.curr_eof,data.length));
-
-       socket.curr_eof = data.length;
-    });
+    if (err) throw err;
+    socket.emit("change",data.substr(socket.curr_eof,data.length));
+    socket.curr_eof = data.length;
+  });
 }
 
-
-app.get('/', function(req, res){
+app.get('/', function(req, res) {
 	res.sendfile('index.html');
 });
 
 
-io.on('connection', function(socket){
-
+io.on('connection', function(socket) {
   socket.curr_eof = 0;
-
   send_delta_log(socket);
 
-  fs.watchFile("log_file.log",function(err,data){
+  fs.watchFile("log_file.log",function(err,data) {
   	send_delta_log(socket);
   });
 
 });
 
-
-
-http.listen(3000, function(){
+http.listen(3000, function() {
   console.log('listening on *:3000');
 });
